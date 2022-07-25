a987841241caaa57766639e94cbfc3539615e5c5
Vinicio Mendes <contatoviniciom@gmail.com>
2022-07-24 22:40:07 -0300

    fix base rn

---
+++ calcfood/src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+import { View, Text, } from 'react-native'; 
+
+export function Welcome(){ 
+    return( 
+        <View> 
+            <Text>Calculadora de Alimentos para Tios e Tias</Text> 
+        </View> 
+    ) 
+}
